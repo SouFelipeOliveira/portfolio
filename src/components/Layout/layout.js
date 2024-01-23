33344882936f7cdded909f5b3674575cb12804ae
Felipe Oliveira <flipeaz342@gmail.com>
2024-01-22 20:46:52 -0400

    spaceships removed

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,13 +1,10 @@
 import './index.scss';
 import Sidebar from '../Sidebar/index';
-import SpaceShip from '../../assets/images/spaceship.gif';
-import SpaceShip2 from '../../assets/images/spaceship2.gif';
+
 const Layout = () => {
     return (
-        <div>
+        <div className='App'>
             <Sidebar />
-            <img className='spaceShip' src={SpaceShip} alt='SpaceShip'/>
-            <img className='spaceShip2' src={SpaceShip2} alt='SpaceShip'/>
         </div>
     )
 };
